0662c16a098a87dc7997c83381be20fa86765e05
ghm7 <pozzisgnosis@outlook.com>
2025-08-03 19:08:45 -0300

    Fix bugs and delete repeated values

--- src/utils/getFactors.ts
+++ src/utils/getFactors.ts
@@ -3,12 +3,16 @@ function getFactors(n: number): Array<number> {
   factors.push(1, n);
 
   for (let i = 2; i <= n; i++) {
+    if (i === n) continue;
+    if (factors.includes(i)) continue;
+
     if (n % i === 0) {
       factors.push(i);
+      factors.push(n / i);
     }
   }
 
-  return factors;
+  return [...new Set(factors.sort((a, b) => a - b))];
 }
 
 export default getFactors;
